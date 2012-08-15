e4677ca010e028a53dd669f162463e861a364641
yueyang <bj.yueyang@gmail.com>
2012-08-15 11:07:30 +0800

    added callback changed: gcc -> clang

--- lib/monitor.c
+++ lib/monitor.c
@@ -22,21 +22,43 @@
 #include <time.h>
 #include <sys/wait.h>
 
-int mni_fork(unsigned int forknum, const char *eval, int count);
-int example(void);
-int check_child(int _count);
+void example_1(void);
+void example_2(void);
+int check_child(void(*ptr)(), int _count);
+int mni_fork(unsigned int forknum, void(*ptr)(), const char *eval, int count);
 
 int
-main ( int argc, char *argv[] ) {
-    mni_fork(2, "", 1);
+main(int argc, char *argv[]) {
+    mni_fork(1, example_2, "", -1);
 
     return EXIT_SUCCESS;
 }				/* ----------  end of function main  ---------- */
 
+void
+example_1(void) {
+    printf("%d\n", getpid());
+    while(1) {
+        sleep(1);
+        printf("examplt 1...\n");
+    }
+
+    return ;
+}
+
+void
+example_2(void) {
+    printf("%d\n", getpid());
+    while(1) {
+        sleep(1);
+        printf("examplt 2...\n");
+    }
+
+    return ;
+}
+
 int
-mni_fork(unsigned int forknum, const char *eval, int count) {
+mni_fork(unsigned int forknum, void(*ptr)(), const char *eval, int count) {
     int i;
-    int rt;
     pid_t pid = 0;
 
     for(i = 0; i < forknum; ++i) {
@@ -49,30 +71,19 @@ mni_fork(unsigned int forknum, const char *eval, int count) {
         }
         if(0 == pid) {
             /*Do something*/
-            rt = example();
-            return rt;
+            (*ptr)();
+            return 0;
         } else {
             /*printf("I am farther,child is %d,index is %d\n", pid, i);*/
         }
     }
 
-    check_child(count);
+    check_child(ptr, count);
     return 0;
 }
 
 int
-example(void) {
-    printf("%d\n", getpid());
-    while(1) {
-        sleep(1);
-        printf("testing...\n");
-    }
-
-    return 0;
-}
-
-int
-check_child(int _count) {
+check_child(void(*ptr)(), int _count) {
     pid_t pid;
 
     for(;;) {
@@ -83,14 +94,14 @@ check_child(int _count) {
         }
         printf("terminal %d\n", pid);
 
-        /*
-         * _count equl 0:
-         *      Don't up child
-         * greater than 0:
-         *      Up _count times
-         * equl -1:
-         *      always up
-         */
+         /*
+          *_count equl 0:
+          *     Don't up child
+          *greater than 0:
+          *     Up _count times
+          *equl -1:
+          *     always up
+          */
         if(0 == _count) {
             printf("chile %d terminal\n", pid);
             return 0;
@@ -99,12 +110,12 @@ check_child(int _count) {
         /*
          *FIXME: multi child and _count > 0
          *           parent return brfore child
-         *           SOwe must kill all child
+         *           SO we must kill all child
          */
         if(_count > 0) {
-            mni_fork(1, "", --_count);
+            mni_fork(1, *ptr, "", --_count);
         } else {
-            mni_fork(1, "", -1);
+            mni_fork(1, *ptr, "", -1);
         }
 
         return 0;
